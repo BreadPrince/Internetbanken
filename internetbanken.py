4d28dfa7918427536eccb872cd4f999e3997357f
BreadPrince <prince.elve.gyllstrom@gmail.com>
2026-03-26 13:58:15 +0100

    Rita upp en sinuskurva

--- internetbanken.py
+++ internetbanken.py
@@ -1,3 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Ritar en sinuskurva
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+plt.plot(x, y)
+plt.title('Sine Wave')
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+plt.grid()
+plt.show()
